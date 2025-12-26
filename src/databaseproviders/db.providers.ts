234d9e4d1a3d18dd6bf13b1d21b962b5520c8e09
saurabh2160 <saurabhmanohar90@gmail.com>
2025-12-26 19:04:31 +0530

    Mongourl removed

--- src/databaseproviders/db.providers.ts
+++ src/databaseproviders/db.providers.ts
@@ -5,7 +5,7 @@ export const databaseProviders = [
   {
     provide: 'MONGO_CLIENT',
     useFactory: async () => {
-      const client = new MongoClient(process.env.mongo_url ?? 'mongodb+srv://saurabhmanohar90:xUuGnRihAZdlpRrv@cluster0.lgkvs.mongodb.net/');
+      const client = new MongoClient(process.env.mongo_url);
       await client.connect();
       console.log('Connected to MongoDB');
       return client;
